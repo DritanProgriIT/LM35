e3cc8215df1a2d4de3ad61c965160be16df27825
Tani <tani@debian.lab.org>
2018-07-01 02:28:48 +0200

    sistemato errore calcoli

--- LM35.cpp
+++ LM35.cpp
@@ -1,16 +1,17 @@
-mer 20 giu 2018 01:01:06 CEST  
+//mer 20 giu 2018 01:01:06 CEST  
 
 #include <Arduino.h>
 #include <LM35.h>
 
 LM35::LM35(int pin,float vPin, int risBit )
 {
-   	_val = analogRead(pin);
+	_pin = pin;
    	_risBit = risBit;
-	_vPin = vPin
+	_vPin = vPin;
 }
 
-float LM35::getTemp(void)
+double LM35::getTemp(void)
 {
-   return (0,01 * _vPin * _val / pow(2,_risBit));
+   return ( _vPin / 0.01 * analogRead(_pin) / pow(2,_risBit));
+ 
 }
--- LM35.h
+++ LM35.h
@@ -1,4 +1,4 @@
-mer 20 giu 2018 01:02:53 CEST 
+//mer 20 giu 2018 01:02:53 CEST 
  
 #ifndef LM35_h
 #define LM35_h
@@ -9,15 +9,15 @@ class LM35
 {
    public:
           
-        LM35(int pin,float vPin int risBit);
-        float getTemp(void);
+        LM35(int pin,float vPin, int risBit);
+        double getTemp(void);
 
   private:
 
         int _pin;
         int _risBit;
         float _val; 
-	float _vPin;	     
+        float _vPin;	     
 };
 
 #endif
